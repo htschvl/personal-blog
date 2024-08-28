d90e942e64c9b4ae710c8ef186275987a045a701
Clarice Hatischvili <claricehatischvili@gmail.com>
2024-08-28 00:10:03 -0300

    outstatic

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  plugins: [require('@tailwindcss/typography')]
+}
+
 /** @type {import("tailwindcss/types").Config } */
 module.exports = {
   content: [
